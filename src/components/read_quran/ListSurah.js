f0ae47fa9642e3dd78a685582556f47e6c963c62
enigma <tommy17002@mail.unpad.ac.id>
2024-08-28 09:27:49 +0700

    trial

--- src/components/read_quran/ListSurah.js
+++ src/components/read_quran/ListSurah.js
@@ -2,9 +2,9 @@ function ListSurah({ listSurah }) {
     return (
       <div className="h-full w-full p-4 overflow-y-auto">
         {listSurah && Array.isArray(listSurah) && listSurah.length > 0 ? (
-          listSurah.map((surah, index) => (
+          listSurah.map((surah) => (
             <ItemSurah
-              key={surah.nomor} // Jangan lupa tambahkan key yang unik
+              key={surah.nomor} // Key yang unik berdasarkan nomor surah
               nomor={surah.nomor}
               namaSurah={surah.nama_latin}
               artiSurah={surah.arti}
@@ -13,26 +13,28 @@ function ListSurah({ listSurah }) {
             />
           ))
         ) : (
-          <p>No Surah available</p> // Pesan yang muncul jika listSurah tidak tersedia
+          <p>No Surah available</p> // Pesan fallback jika tidak ada surah yang tersedia
         )}
       </div>
     );
-  }  
-
-export default ListSurah
-
-function ItemSurah({nomor, namaSurah, artiSurah, tempatTurunSurah, jumlahAyat})
-{
+  }
+  
+  export default ListSurah;
+  
+  function ItemSurah({ nomor, namaSurah, artiSurah, tempatTurunSurah, jumlahAyat }) {
     return (
-        <div className="h-24 w-full border-b-2 border-teal-700 flex mb-4">
-            <div className="basis-12 flex justify-end mr-4"> 
-                <h3 className="text-2xl font-bold text-teal-700">{nomor}</h3>
-            </div>
-            <div className="flex flex-col">
-                <h3 className="text-2xl font-bold text-teal-700">{namaSurah}</h3>
-                <h5 className="text-base font-bold text-gray-500 ">{artiSurah}</h5>
-                <span className="text-base font-bold text-gray-500 ">{tempatTurunSurah}, {jumlahAyat} ayat</span>    
-            </div>
+      <div className="h-24 w-full border-b-2 border-teal-700 flex mb-4">
+        <div className="basis-12 flex justify-end mr-4">
+          <h3 className="text-2xl font-bold text-teal-700">{nomor}</h3>
         </div>
-    )
-}
+        <div className="flex flex-col">
+          <h3 className="text-2xl font-bold text-teal-700">{namaSurah}</h3>
+          <h5 className="text-base font-bold text-gray-500">{artiSurah}</h5>
+          <span className="text-base font-bold text-gray-500">
+            {tempatTurunSurah}, {jumlahAyat} ayat
+          </span>
+        </div>
+      </div>
+    );
+  }
+  
